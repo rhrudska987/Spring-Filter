6b7b9042781a6abf9e217abf66fda272939243f1
KyungNam Ko <rhrudska987@naver.com>
2022-02-22 18:47:38 +0900

    2022.02.22 Study

---
+++ src/main/java/com/example/filter/dto/User.java
@@ -0,0 +1,13 @@
+package com.example.filter.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor //기본생성자
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+
+}
